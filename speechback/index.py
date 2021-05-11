e92e4d89ebcaf0a362ea4088be7373f46e0f5678
Jan Oldrich Kruza <sixtease+github@gmail.com>
2021-05-11 19:59:29 +0200

    initial commit

---
+++ speechback/index.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import cherrypy
+import sys
+from subprocess import run
+from tempfile import TemporaryDirectory
+from shutil import rmtree
+from os import mkdir
+import json
+
+class SpeechBack(object):
+  @cherrypy.expose
+  def submit_audio(self, audio):
+    if cherrypy.request.method == 'OPTIONS':
+      cherrypy.response.headers['Access-Control-Allow-Origin'] = '*'
+
+    request_id = str(cherrypy.request.unique_id)
+    workdir = '/tmp/%s' % (request_id)
+    mkdir(workdir)
+    unformatted_audio_fn = '%s/%s' % (workdir, 'orig_audio')
+    audio_fn = '%s/%s' % (workdir, 'audio.wav')
+
+    with open(unformatted_audio_fn, 'wb') as f:
+      while True:
+        data = audio.file.read(8192)
+        if not data:
+          break
+        f.write(data)
+
+    run(['sox', unformatted_audio_fn, audio_fn, 'remix', '-', 'rate', '16k'])
+    
+    cherrypy.response.headers['Content-Type'] = 'text/json'
+    return json.dumps({ 'status': 'OK', 'session': request_id })
+
+cherrypy.config.update({
+  'server.socket_host': '0.0.0.0',
+  'server.socket_port': 8081,
+})
+
+cherrypy.quickstart(SpeechBack())
